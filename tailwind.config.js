b3b6f1e30683527ac085e268825401c0760a0aba
Irakli Iremashvili <i_iremashvili@cu.edu.ge>
2021-04-19 15:50:18 +0400

    add tailwind to front

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const { guessProductionMode } = require("@ngneat/tailwind");
+
+module.exports = {
+    prefix: '',
+    purge: {
+      enabled: guessProductionMode(),
+      content: [
+        './apps/**/*.{html,ts}',
+        './libs/**/*.{html,ts}',
+      ]
+    },
+    darkMode: 'media', // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+};
